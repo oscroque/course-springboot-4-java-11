455e5b1e6c8e3f377690568069f564e9e13c2042
Oscar Roque <oscroque@gmail.com>
2020-08-31 17:15:03 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,93 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String passsword;
+	
+	public User() {
+		
+	}
+
+	public User(Long id, String name, String email, String phone, String passsword) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.passsword = passsword;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPasssword() {
+		return passsword;
+	}
+
+	public void setPasssword(String passsword) {
+		this.passsword = passsword;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		return true;
+	}
+	
+	
+}
